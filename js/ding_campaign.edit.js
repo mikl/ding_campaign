58b70b5d6a1411ec188a98fa7110fc431d9e1909
Mikkel Hoegh <mikkel@hoegh.org>
2010-01-14 14:38:30 +0100

    Setting up Ding Campaign as a separate repo.

---
+++ js/ding_campaign.edit.js
@@ -0,0 +1,26 @@
+// $Id$
+
+Drupal.behaviors.dingCampaignEdit = function () {
+  $('#node-form')
+    .find('.campaign-type-select input')
+      .click(function () {
+          if ($(this).val() == 'text-only') {
+            $('#node-form')
+              .find('.body-field-wrapper').show('fast').end()
+              .find('#edit-field-campaign-image-0-ahah-wrapper').hide('fast').end()
+            .end();
+          }
+          else if ($(this).val() == 'image-only') {
+            $('#node-form')
+              .find('.body-field-wrapper').hide('fast').end()
+              .find('#edit-field-campaign-image-0-ahah-wrapper').show('fast').end()
+            .end();
+          }
+        })
+      .end()
+    // Now that we have a click handler, trigger it on the already
+    // selected radio to have the inital hiding taking place.
+    .find('.campaign-type-select input[checked]').click()
+    .end();
+};
+
---
+++ js/ding_campaign.rules.js
@@ -0,0 +1,41 @@
+// $Id$
+
+Drupal.behaviors.dingCampaignRules = function () {
+  $('#campaign-rules')
+    .find('.campaign-rule-wrap')
+      .each(function (i) {
+          var type = $(this).find('select').val();
+
+          $(this).find('.form-text')
+            // Remove the current autocomplete bindings.
+            .unbind()
+            // And remove the autocomplete class
+            .removeClass('form-autocomplete')
+          .end()
+          // For those type selectors not processed yet, set up the
+          // change event to trigger this behavior, and add the
+          // processed indicator class.
+          .find('select:not(.dingcampaignrules-processed)')
+            .addClass('dingcampaignrules-processed')
+            .change(Drupal.behaviors.dingCampaignRules)
+          .end();
+  
+          if (type == 'page' || type == 'library' || type == 'taxonomy') {
+            $(this).find('input.autocomplete')
+              .removeClass('autocomplete-processed')
+              .val(Drupal.settings.dingCampaignRules.autocompleteUrl + type)
+            .end()
+            .find('.form-text')
+              .addClass('form-autocomplete');
+            Drupal.behaviors.autocomplete(this);
+          }
+
+          if (type == 'generic') {
+            $(this).find("div:has(input.form-text)").hide();
+          }
+          else {
+            $(this).find("div:has(input.form-text)").show();
+          }
+      });
+};
+
